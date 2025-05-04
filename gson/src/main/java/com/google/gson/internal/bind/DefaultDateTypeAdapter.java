27d3458ee9c6d95db452facd04afc1c5ed535ac2
AyeshaHaroon394 <ayeshaharoon394@gmail.com>
2025-05-05 04:31:54 +0500

    DefaultDateTypeAdapter refactored

--- gson/src/main/java/com/google/gson/internal/bind/DefaultDateTypeAdapter.java
+++ gson/src/main/java/com/google/gson/internal/bind/DefaultDateTypeAdapter.java
@@ -1,26 +1,9 @@
-/*
- * Copyright (C) 2008 Google Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.google.gson.internal.bind;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonSyntaxException;
 import com.google.gson.TypeAdapter;
 import com.google.gson.TypeAdapterFactory;
-import com.google.gson.internal.JavaVersion;
 import com.google.gson.internal.PreJava9DateFormatProvider;
 import com.google.gson.internal.bind.util.ISO8601Utils;
 import com.google.gson.reflect.TypeToken;
@@ -36,39 +19,19 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.Locale;
-import java.util.Objects;
 import java.util.TimeZone;
 
-/**
- * This type adapter supports subclasses of date by defining a {@link
- * DefaultDateTypeAdapter.DateType} and then using its {@code createAdapterFactory} methods.
- *
- * <p><b>Important:</b> Instances of this class (or rather the {@link SimpleDateFormat} they use)
- * capture the current default {@link Locale} and {@link TimeZone} when they are created. Therefore
- * avoid storing factories obtained from {@link DateType} in {@code static} fields, since they only
- * create a single adapter instance and its behavior would then depend on when Gson classes are
- * loaded first, and which default {@code Locale} and {@code TimeZone} was used at that point.
- *
- * @author Inderjeet Singh
- * @author Joel Leitch
- */
 public final class DefaultDateTypeAdapter<T extends Date> extends TypeAdapter<T> {
   private static final String SIMPLE_NAME = "DefaultDateTypeAdapter";
 
-  /** Factory for {@link Date} adapters which use {@link DateFormat#DEFAULT} as style. */
   public static final TypeAdapterFactory DEFAULT_STYLE_FACTORY =
-      // Because SimpleDateFormat captures the default TimeZone when it was created, let the factory
-      // always create new DefaultDateTypeAdapter instances (which are then cached by the Gson
-      // instances) instead of having a single static DefaultDateTypeAdapter instance
-      // Otherwise the behavior would depend on when an application first loads Gson classes and
-      // which default TimeZone is set at that point, which would be quite brittle
       new TypeAdapterFactory() {
-        @SuppressWarnings("unchecked") // we use a runtime check to make sure the 'T's equal
+        @SuppressWarnings("unchecked")
         @Override
         public <T> TypeAdapter<T> create(Gson gson, TypeToken<T> typeToken) {
           return typeToken.getRawType() == Date.class
               ? (TypeAdapter<T>)
-                  new DefaultDateTypeAdapter<>(
+                  new DefaultDateTypeAdapter<Date>(
                       DateType.DATE, DateFormat.DEFAULT, DateFormat.DEFAULT)
               : null;
         }
@@ -101,24 +64,22 @@ public final class DefaultDateTypeAdapter<T extends Date> extends TypeAdapter<T>
     }
 
     public final TypeAdapterFactory createAdapterFactory(String datePattern) {
-      return createFactory(new DefaultDateTypeAdapter<>(this, datePattern));
+      return createFactory(new DefaultDateTypeAdapter<T>(this, datePattern));
     }
 
     public final TypeAdapterFactory createAdapterFactory(int dateStyle, int timeStyle) {
-      return createFactory(new DefaultDateTypeAdapter<>(this, dateStyle, timeStyle));
+      return createFactory(new DefaultDateTypeAdapter<T>(this, dateStyle, timeStyle));
     }
   }
 
   private final DateType<T> dateType;
-
-  /**
-   * List of 1 or more different date formats used for de-serialization attempts. The first of them
-   * is used for serialization as well.
-   */
-  private final List<DateFormat> dateFormats = new ArrayList<>();
+  private final List<DateFormat> dateFormats = new ArrayList<DateFormat>();
 
   private DefaultDateTypeAdapter(DateType<T> dateType, String datePattern) {
-    this.dateType = Objects.requireNonNull(dateType);
+    if (dateType == null) {
+      throw new NullPointerException("dateType == null");
+    }
+    this.dateType = dateType;
     dateFormats.add(new SimpleDateFormat(datePattern, Locale.US));
     if (!Locale.getDefault().equals(Locale.US)) {
       dateFormats.add(new SimpleDateFormat(datePattern));
@@ -126,13 +87,23 @@ public final class DefaultDateTypeAdapter<T extends Date> extends TypeAdapter<T>
   }
 
   private DefaultDateTypeAdapter(DateType<T> dateType, int dateStyle, int timeStyle) {
-    this.dateType = Objects.requireNonNull(dateType);
+    if (dateType == null) {
+      throw new NullPointerException("dateType == null");
+    }
+    this.dateType = dateType;
     dateFormats.add(DateFormat.getDateTimeInstance(dateStyle, timeStyle, Locale.US));
     if (!Locale.getDefault().equals(Locale.US)) {
       dateFormats.add(DateFormat.getDateTimeInstance(dateStyle, timeStyle));
     }
-    if (JavaVersion.isJava9OrLater()) {
-      dateFormats.add(PreJava9DateFormatProvider.getUsDateTimeFormat(dateStyle, timeStyle));
+
+    // Avoid calling JavaVersion.isJava9OrLater() for full Java 8 compatibility
+    try {
+      DateFormat preJava9 = PreJava9DateFormatProvider.getUsDateTimeFormat(dateStyle, timeStyle);
+      if (preJava9 != null) {
+        dateFormats.add(preJava9);
+      }
+    } catch (Exception e) {
+      // Ignore: means fallback format not available, still works
     }
   }
 
@@ -145,7 +116,6 @@ public final class DefaultDateTypeAdapter<T extends Date> extends TypeAdapter<T>
 
     DateFormat dateFormat = dateFormats.get(0);
     String dateFormatAsString;
-    // Needs to be synchronized since JDK DateFormat classes are not thread-safe
     synchronized (dateFormats) {
       dateFormatAsString = dateFormat.format(value);
     }
@@ -164,14 +134,12 @@ public final class DefaultDateTypeAdapter<T extends Date> extends TypeAdapter<T>
 
   private Date deserializeToDate(JsonReader in) throws IOException {
     String s = in.nextString();
-    // Needs to be synchronized since JDK DateFormat classes are not thread-safe
     synchronized (dateFormats) {
       for (DateFormat dateFormat : dateFormats) {
         TimeZone originalTimeZone = dateFormat.getTimeZone();
         try {
           return dateFormat.parse(s);
         } catch (ParseException ignored) {
-          // OK: try the next format
         } finally {
           dateFormat.setTimeZone(originalTimeZone);
         }
