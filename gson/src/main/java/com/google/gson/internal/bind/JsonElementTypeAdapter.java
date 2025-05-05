19495a0686dec9477d3350b059bd3b16d530ad65
AyeshaHaroon394 <ayeshaharoon394@gmail.com>
2025-05-05 05:08:14 +0500

    JsonElementTypeAdapter refactored

--- gson/src/main/java/com/google/gson/internal/bind/JsonElementTypeAdapter.java
+++ gson/src/main/java/com/google/gson/internal/bind/JsonElementTypeAdapter.java
@@ -1,27 +1,6 @@
-/*
- * Copyright (C) 2024 Google Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.google.gson.internal.bind;
 
-import com.google.gson.JsonArray;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonNull;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonPrimitive;
-import com.google.gson.TypeAdapter;
+import com.google.gson.*;
 import com.google.gson.internal.LazilyParsedNumber;
 import com.google.gson.stream.JsonReader;
 import com.google.gson.stream.JsonToken;
@@ -37,12 +16,8 @@ class JsonElementTypeAdapter extends TypeAdapter<JsonElement> {
 
   private JsonElementTypeAdapter() {}
 
-  /**
-   * Tries to begin reading a JSON array or JSON object, returning {@code null} if the next element
-   * is neither of those.
-   */
-  private JsonElement tryBeginNesting(JsonReader in, JsonToken peeked) throws IOException {
-    switch (peeked) {
+  private JsonElement tryBeginNesting(JsonReader in, JsonToken token) throws IOException {
+    switch (token) {
       case BEGIN_ARRAY:
         in.beginArray();
         return new JsonArray();
@@ -54,57 +29,46 @@ class JsonElementTypeAdapter extends TypeAdapter<JsonElement> {
     }
   }
 
-  /** Reads a {@link JsonElement} which cannot have any nested elements */
-  private JsonElement readTerminal(JsonReader in, JsonToken peeked) throws IOException {
-    switch (peeked) {
+  private JsonElement readTerminal(JsonReader in, JsonToken token) throws IOException {
+    switch (token) {
       case STRING:
         return new JsonPrimitive(in.nextString());
       case NUMBER:
-        String number = in.nextString();
-        return new JsonPrimitive(new LazilyParsedNumber(number));
+        return new JsonPrimitive(new LazilyParsedNumber(in.nextString()));
       case BOOLEAN:
         return new JsonPrimitive(in.nextBoolean());
       case NULL:
         in.nextNull();
         return JsonNull.INSTANCE;
       default:
-        // When read(JsonReader) is called with JsonReader in invalid state
-        throw new IllegalStateException("Unexpected token: " + peeked);
+        throw new IllegalStateException("Unexpected token: " + token);
     }
   }
 
   @Override
   public JsonElement read(JsonReader in) throws IOException {
-    // Optimization if value already exists as JsonElement
     if (in instanceof JsonTreeReader) {
       return ((JsonTreeReader) in).nextJsonElement();
     }
 
-    // Either JsonArray or JsonObject
-    JsonElement current;
-    JsonToken peeked = in.peek();
-
-    current = tryBeginNesting(in, peeked);
-    if (current == null) {
-      return readTerminal(in, peeked);
+    JsonToken token = in.peek();
+    JsonElement root = tryBeginNesting(in, token);
+    if (root == null) {
+      return readTerminal(in, token);
     }
 
     Deque<JsonElement> stack = new ArrayDeque<>();
 
+    JsonElement current = root;
+
     while (true) {
       while (in.hasNext()) {
-        String name = null;
-        // Name is only used for JSON object members
-        if (current instanceof JsonObject) {
-          name = in.nextName();
-        }
-
-        peeked = in.peek();
-        JsonElement value = tryBeginNesting(in, peeked);
-        boolean isNesting = value != null;
+        String name = (current instanceof JsonObject) ? in.nextName() : null;
+        token = in.peek();
+        JsonElement value = tryBeginNesting(in, token);
 
         if (value == null) {
-          value = readTerminal(in, peeked);
+          value = readTerminal(in, token);
         }
 
         if (current instanceof JsonArray) {
@@ -113,13 +77,12 @@ class JsonElementTypeAdapter extends TypeAdapter<JsonElement> {
           ((JsonObject) current).add(name, value);
         }
 
-        if (isNesting) {
+        if (value instanceof JsonArray || value instanceof JsonObject) {
           stack.addLast(current);
           current = value;
         }
       }
 
-      // End current element
       if (current instanceof JsonArray) {
         in.endArray();
       } else {
@@ -128,10 +91,9 @@ class JsonElementTypeAdapter extends TypeAdapter<JsonElement> {
 
       if (stack.isEmpty()) {
         return current;
-      } else {
-        // Continue with enclosing element
-        current = stack.removeLast();
       }
+
+      current = stack.removeLast();
     }
   }
 
@@ -139,33 +101,44 @@ class JsonElementTypeAdapter extends TypeAdapter<JsonElement> {
   public void write(JsonWriter out, JsonElement value) throws IOException {
     if (value == null || value.isJsonNull()) {
       out.nullValue();
-    } else if (value.isJsonPrimitive()) {
-      JsonPrimitive primitive = value.getAsJsonPrimitive();
-      if (primitive.isNumber()) {
-        out.value(primitive.getAsNumber());
-      } else if (primitive.isBoolean()) {
-        out.value(primitive.getAsBoolean());
-      } else {
-        out.value(primitive.getAsString());
-      }
+      return;
+    }
 
+    if (value.isJsonPrimitive()) {
+      writePrimitive(out, value.getAsJsonPrimitive());
     } else if (value.isJsonArray()) {
-      out.beginArray();
-      for (JsonElement e : value.getAsJsonArray()) {
-        write(out, e);
-      }
-      out.endArray();
-
+      writeArray(out, value.getAsJsonArray());
     } else if (value.isJsonObject()) {
-      out.beginObject();
-      for (Map.Entry<String, JsonElement> e : value.getAsJsonObject().entrySet()) {
-        out.name(e.getKey());
-        write(out, e.getValue());
-      }
-      out.endObject();
-
+      writeObject(out, value.getAsJsonObject());
     } else {
       throw new IllegalArgumentException("Couldn't write " + value.getClass());
     }
+  }
+
+  private void writePrimitive(JsonWriter out, JsonPrimitive primitive) throws IOException {
+    if (primitive.isNumber()) {
+      out.value(primitive.getAsNumber());
+    } else if (primitive.isBoolean()) {
+      out.value(primitive.getAsBoolean());
+    } else {
+      out.value(primitive.getAsString());
+    }
+  }
+
+  private void writeArray(JsonWriter out, JsonArray array) throws IOException {
+    out.beginArray();
+    for (JsonElement element : array) {
+      write(out, element);
+    }
+    out.endArray();
+  }
+
+  private void writeObject(JsonWriter out, JsonObject object) throws IOException {
+    out.beginObject();
+    for (Map.Entry<String, JsonElement> entry : object.entrySet()) {
+      out.name(entry.getKey());
+      write(out, entry.getValue());
+    }
+    out.endObject();
   }
 }
